75c385cc58f148d07393cede494b98ee43773ac7
WineBewes <wine.bewes@gmail.com>
2014-08-18 13:24:57 +0200

    continued

---
+++ Project/Examples/ReSharperEdges.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Examples
+{
+	public class ReSharperEdges
+	{
+		public void Kijk_Altijd_Na_Of_ReSharper_Beter_Leesbare_Code_Oplevert()
+		{
+			//ReSharper stelt "collection initializer voor : OK
+			var bestaandeActies = new List<string>();
+			bestaandeActies.Add("actieA");
+			bestaandeActies.Add("actieB");
+			bestaandeActies.Add("actieC");
+
+			var behandeldeActies = new List<string> { "actieA", "actieC" };
+
+			//ReSharper stelt voor : "Convert part of body into LINQ-expression" : OK ???
+			foreach (var bestaandeActie in bestaandeActies)
+			{
+				if (!behandeldeActies.Contains(bestaandeActie))
+				{
+					throw new Exception(string.Format("Bestaande actie {0} wordt niet behandeld.", bestaandeActie));
+				}
+			}
+		}
+	}
+}
